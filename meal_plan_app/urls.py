48f7a7dd5c70867ea213fa048a6ebfecdf544414
smatthe4 <98066285+smatthe4@users.noreply.github.com>
2024-04-27 15:05:15 -0600

    buttons work, no login yet, will try to pull from the other branch...

--- meal_plan_app/urls.py
+++ meal_plan_app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     # name='index' parameter is to dynamically create url
     # example in html <a href="{% url 'index' %}">Home</a>.
     path('', views.index, name='index'),
+    path('mealist', views.mealist, name='meallist'),
 ]
--- meal_plan_app/views.py
+++ meal_plan_app/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'meal_plan_app/index.html')
+
+def mealist(request):
+    return render(request, 'meal_plan_app/mealistwip.html')
